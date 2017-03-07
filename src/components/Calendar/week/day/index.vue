345886dc5a396974d5e9404c615acb9b3bf9e090
Eric Lau <elau002@gmail.com>
2017-03-06 19:23:35 -0800

    Adding onClick to display slot of clicked element

--- src/components/Calendar/week/day/index.vue
+++ src/components/Calendar/week/day/index.vue
@@ -44,46 +44,39 @@ import hour from './hour';
         this.$parent.$emit('selected', JSON.stringify([day, hour]));
       },
     },
-    // events: {
-    //   selected: () => {
-    //     console.log('hello');
-    //     console.log(this.$data.selected);
-    //   }
-    // }
   }
 </script>
 
 
-<!-- CSS Code: Place this code in the document's head (between the 'head' tags) -->
 <style>
-table.dayTable {
-  width: 10%;
-  background-color: #ffffff;
-  border-collapse: collapse;
-  border-right: 1px solid black;
-  color: #000000;
-  float: left;
-  text-align: center;
-}
+  table.dayTable {
+    width: 10%;
+    background-color: #ffffff;
+    border-collapse: collapse;
+    border-right: 1px solid black;
+    color: #000000;
+    float: left;
+    text-align: center;
+  }
 
-table.dayTable td, table.dayTable th {
-  padding: 3px;
-  height: 20px;
-}
+  table.dayTable td, table.dayTable th {
+    padding: 3px;
+    height: 20px;
+  }
 
-table.dayTable td:hover {
-  background: #F1F1F1;
-  cursor: default;
-  box-shadow: 1px 2px 2px #705848;
-}
+  table.dayTable td:hover {
+    background: #F1F1F1;
+    cursor: default;
+    box-shadow: 1px 2px 2px #705848;
+  }
 
-table.dayTable th {
-  cursor: default;
-}
+  table.dayTable th {
+    cursor: default;
+  }
 
-table.dayTable thead {
-  background-color: #white;
-}
+  table.dayTable thead {
+    background-color: #white;
+  }
 </style>
 
 
